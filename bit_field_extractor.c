ef7f7d11a71c2162f52ba5330404646009432cad
raaaf-404 <rafaeltuan404@gmail.com>
2026-06-28 21:49:10 +0800

    exercise #1.3

---
+++ bit_field_extractor.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+uint32_t extract_field(uint32_t reg, uint8_t start_bit, uint8_t width);
+insert_field(uint32_t reg, uint8_t start, uint8_t width, uint32_t value);
+
+int main(){
+
+    return 0;
+}
+
+uint32_t extract_field(uint32_t reg, uint8_t start_bit, uint8_t width) 
+{
+    uint32_t field = (reg >> start_bit)  & (1 << width) - 1;
+    return field;
+}
+
+insert_field(uint32_t reg, uint8_t start, uint8_t width, uint32_t value) 
+{
+    
+}
