090e3644622f60290326ea1a53abb050b2f03b2f
raaaf-404 <rafaeltuan404@gmail.com>
2026-06-29 21:00:42 +0800

    exercise 1.3 insert field #issue 1

--- bit_field_extractor.c
+++ bit_field_extractor.c
@@ -15,6 +15,7 @@ uint32_t extract_field(uint32_t reg, uint8_t start_bit, uint8_t width)
 }
 
 insert_field(uint32_t reg, uint8_t start, uint8_t width, uint32_t value) 
-{
-    
+{ 
+    uint32_t reg_clear = reg & ((1 << width) - 1) << (start + 1);
+    reg = reg_clear | value << start;
 }
